5af3e11b9661af245d4900efaaa99fab81651626
Aazan Noor Khuwaja <aazan-noor-khuwaja@Hp-G3.lan>
2026-02-06 11:40:45 +0500

    added something

--- car_simulation_while_loops.py
+++ car_simulation_while_loops.py
@@ -1,18 +1,27 @@
 print('...Game Started...\n')
-instruction=input('''Start-To start the Car..\n
-Stop-To stop the Car...\n
-Quit-To exit from the game..!!\n''')
-
-
+started=False
+stopped=False
+instruction=input('> ')
 while(1):
     if(instruction=='Start' or instruction=='start'):
-        print('Car started...\n')
+        if(started):
+            print('Car is already started..\n')
+        else:
+            started=True
+            print('Car started...\n')
+            
     elif(instruction=='Stop' or instruction=='stop'):
-        print('Car stopped...\n')
+        if(stopped):
+            print('Hey car is stopped!! what are you doing?\n')
+        else:
+            stopped=True
+            print('Car stopped...\n')
+            
     elif(instruction=='Help' or instruction=='help'):
-        print('''Start-To start the Car..\n
-Stop-To stop the Car...\n
-Quit-To exit from the game..!!\n''')
+        print('''Start-> To start the Car..\n
+Stop-> To stop the Car...\n
+Quit-> To exit from the game..!!\n''')
+        
     elif(instruction== 'Quit' or instruction == 'quit'):
         print('Leaving game...\n')
         break
